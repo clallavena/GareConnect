d11164a06032befa76cb13d914917e3ce3e01993
clallavena <allavenaclementcg@gmail.com>
2024-02-02 19:17:38 +0100

    Add some comments

--- lib/src/controllers/interfaces/pto_interface.dart
+++ lib/src/controllers/interfaces/pto_interface.dart
@@ -2,5 +2,7 @@ import 'package:gare_connect/src/model/pto_auto_model.dart';
 import 'package:http/http.dart' as http;
 
 abstract class PtoAutoComplete {
+  /// This method is used to search a pto object with a searchkey.
+  /// It's possible to search any kind of object by using the object type.
   Future<PtoAuto?> getPtoAutocomplete(http.Client http, String searchKey, String? objectType, {int depth = 1});
 }
--- lib/src/controllers/interfaces/stop_areas_interface.dart
+++ lib/src/controllers/interfaces/stop_areas_interface.dart
@@ -2,6 +2,8 @@ import 'package:gare_connect/src/model/pto_model.dart';
 import 'package:http/http.dart' as http;
 
 abstract class StopAreasInterface{
+  /// Used to get all stop areas available.
   Future<Pto?> getStopAreas(http.Client http);
+  /// Used to get stop areas with its exact name
   Future<Pto?> getStopAreasByExactName(http.Client http, String name, {int depth = 1});
 }
